6da05841d79eb9f41d1bbb48f143ca2052aaa47e
cferko <cferko@domeyard.com>
2016-12-18 16:23:15 -0500

    REV: some fixes

--- setup.py
+++ setup.py
@@ -6,14 +6,27 @@ Created on Sun Dec 18 10:48:38 2016
 """
 
 import soundfile as sf
-import requests, pickle, os
+import requests, pickle, os, numpy as np
 LOOKUP_DICT = pickle.load(open("file_lookup.pkl", "r"))
 
 import IPython
 
-def listen(audio, rate):
-    return IPython.display.Audio(audio, rate = rate)
+def standardize(old_rate, audio):
+    if audio.ndim == 2:
+        audio = audio.mean(axis=1)
+    
+    if old_rate == 44100:
+        return audio
 
+    n = len(audio)
+    upsample_factor = float(old_rate)/44100
+    new_audio = np.interp(np.arange(0, n, upsample_factor),
+                          np.arange(0, n, 1), audio)
+
+    return new_audio
+
+def listen(audio):
+    return IPython.display.Audio(audio, rate = 44100)
 
 def download_file(url):
     """Helper function to download a file with requests
@@ -84,7 +97,7 @@ def get_song(genre, number):
         
     data, samplerate = sf.read(name)
     
-    return data
+    return standardize(samplerate, data)
 
 def get_sample():
     closer = "https://www.dropbox.com/s/b7lcd4s7pvcroie/closer.wav?dl=1"
@@ -93,7 +106,7 @@ def get_sample():
         download_file(closer)
     d1, s1 = sf.read("closer.wav")
 
-    return d1, s1
+    return standardize(s1, d1)
     
 if __name__ == "__main__":
     closer, rate = get_sample()
